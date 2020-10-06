6ba4ada81559f6b31183147adf12d4387759e7a4
Mac <xuromeo@gmail.com>
2020-10-05 21:06:28 -0700

    up

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,27 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement+1<20){
+    item.enhancement +=1
+  }
+  return item
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement<15){
+    item.durability-=5
+  }
+  if(item.enhancement>=15){
+    item.durability-=10
+  }
+  if(item.enhancement>16){
+    item.enhancement-=1
+  }
+  return item
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item,durability:100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,50 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const testDataOne = {
+ name:'one',
+ durability:50,
+ enhancement:10
+}
+
+const testDataTwo = {
+    name:'two',
+    durability:60,
+    enhancement:20
+}
+
+describe('test',()=>{
+    it('to see if the test working',()=>{
+ expect(true).toBe(true)
+    })
+})
+
+describe('test enhancer function',()=>{
+    it('repair',()=>{
+    const result = enhancer.repair(testDataOne)
+    expect(result.durability).toBe(100)
+    })
+
+    it('success less than 20',()=>{
+    const result = enhancer.success(testDataOne)
+    expect(result.enhancement).toBe(11)
+    })
+
+    it('success at 20',()=>{
+    const result = enhancer.success(testDataTwo)
+    expect(result.enhancement).toBe(20)
+    expect(result.durability).toBe(60)
+    })
+
+    it('fail and enhancement less than 15',()=>{
+    const result = enhancer.fail(testDataOne)
+    expect(result.durability).toBe(45)
+    })
+
+    it('enhancement is 15 or more',()=>{
+    const result = enhancer.fail(testDataTwo)
+    expect(result.durability).toBe(50)
+    expect(result.enhancement).toBe(19)
+    })
+
+    
+})
